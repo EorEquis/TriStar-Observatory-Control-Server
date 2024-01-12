d045b2339466680ff1c69fa19d9830b645bc89cc
EorEquis <eeyore@stuffupthere.com>
2024-01-12 13:46:04 -0600

    Undefine DEBUG, clean up unused defines, initial deployment

--- Arduino/TriStar_Obs_Control_Server/Config.h
+++ Arduino/TriStar_Obs_Control_Server/Config.h
@@ -14,8 +14,6 @@ VARIABLE                             VALUE           COMMENT                    
 int resetWatchdogEvery = 5000;                  // Number of miliseconds to reset the watchdog, since 8s is WDT max, and we poll less often
 
 // Uncomment these lines to enable features
-#define DEBUG                                   // Debug info to serial monitor
+//#define DEBUG                                   // Debug info to serial monitor
 //#define DELAY_FOR_SWITCH                      // Delay 10s at start of sketch to wait for network switch to come up
-//#define WX_USE_RSEND                          // Use rain sensor's digital output?  If defined, then RSEND of 1 is           UNSAFE
-//#define USE_ALLSKY_AI                         // Make use of allskyai.com's allsky AI for safety
 //#define USEBUTTON                             // Uncomment to allow use of manual roof open/close button
